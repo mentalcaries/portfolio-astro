54f64afd5889a31f9b2b628e09eef3bb269ded5b
mentalcaries <neexis@gmail.com>
2025-02-14 09:52:17 -0400

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+
   server: {
     port: 3002,
   },
+
+  output: "server",
+  adapter: cloudflare(),
 });
