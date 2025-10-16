b7f31e4d35c8421733bbbf59b7ec22080d68b2d8
mentalcaries <neexis@gmail.com>
2025-10-16 18:33:08 -0400

    update og tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-
+  site: "https://jaggernauth.dev",
   server: {
     port: 3002,
   },
